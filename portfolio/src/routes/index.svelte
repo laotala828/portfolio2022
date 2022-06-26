895539b45d9cbcf8dfa62e0bce12e8b41d0263a8
laotala828 <laotala@gmail.com>
2022-06-26 00:11:49 -0700

    content

--- portfolio/src/routes/index.svelte
+++ portfolio/src/routes/index.svelte
@@ -1,5 +1,70 @@
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="mx-auto max-w-3xl gap-10 flex flex-col py-12">
+    <div class="flex gap-6 items-baseline">
+        <div class="text-sm text-neutral-400 flex-grow-0 flex-shrink-0 w-16 leading-7 text-right"></div>
+        <div class="flex-grow">
+            <h1 class=" text-5xl font-serif font-bold text-neutral-800">Lasifu Ta<span class="text-sm ml-4 font-sans font-normal">Product designer</span></h1>
+            <div class="flex gap-8 mt-4 text-sm">
+                <p>2066606221</p>
+                <span class="font-light">|</span>
+                <p>laotala@gmail.com</p>
+                <span class="font-light">|</span>
+                <p>linkedin/lasifuta</p>
+            </div>
+        </div>
+    </div>
+    <div class="flex gap-6 items-baseline">
+        <div class="text-sm text-neutral-400 flex-grow-0 flex-shrink-0 w-16 leading-7 text-right relative">Present<br>Nov 2021
+            <div class="absolute -left-28 top-0 text-neutral-800 font-bold text-right">experience</div>
+        </div>
+        <div class="flex-grow">
+            <h2 class="text-xl font-serif font-bold text-neutral-800">Product designer, Rewatch</h2>
+            <p class="text-sm text-neutral-400 leading-7">Designed the Comment feature to enable async communication on video.<br/>
+                Designed the Series, Chapter, and Tagging features to find content quickly. <br/>
+                Coded the front-end user interface with HTML, CSS, Javascript, and Rails.<br/>
+                Increased Lighthouse accessibility score of the web clients by 40%. </p>
+        </div>
+    </div>
+    <div class="flex gap-6 items-baseline">
+        <div class="text-sm text-neutral-400 flex-grow-0 flex-shrink-0 w-16 leading-7 text-right">Nov 2021<br>Sep 2019</div>
+        <div class="flex-grow">
+            <h2 class="text-xl font-serif font-bold text-neutral-800">UX designer, Amazon</h2>
+            <p class="text-sm text-neutral-400 leading-7">Led the end-to-end design for AWS BugBust, an AI-enabled gamification experience for bug-fixing. <br/>
+                Designed Amazon DevOps Guru, an AI-driven service to find and predict anomalies.<br/>
+                Modernized the UI of  Amazon Lex, a conversational AI and chatbot service.<br/>
+                Designed AWS Panorama, a computer vision application installed at the edge.</p>
+        </div>
+    </div>
+    <div class="flex gap-6 items-baseline">
+        <div class="text-sm text-neutral-400 flex-grow-0 flex-shrink-0 w-16 leading-7 text-right">Sep 2019<br>Jun 2015</div>
+        <div class="flex-grow">
+            <h2 class="text-xl font-serif font-bold text-neutral-800">Product designer, Microsoft</h2>
+            <p class="text-sm text-neutral-400 leading-7">Led the design for Whiteboard mobile and web clients.<br/>
+                Designed interactions for different input methods and hardware devices.<br/>
+                Integrated Whiteboard with Microsoft Office and Teams ecosystem.<br/>
+                Defined user models to enable secure and smooth cross-tenant collaborations.<br/>
+                Visualized health data to deliver actionable insights on web and mobile devices.</p>
+        </div>
+    </div>
+    <div class="flex gap-6 items-baseline">
+        <div class="text-sm text-neutral-400 flex-grow-0 flex-shrink-0 w-16 leading-7 text-right relative">May 2015<br>Sep 2013
+            <div class="absolute -left-28 top-0 text-neutral-800 font-bold text-right">education</div>
+        </div>
+        <div class="flex-grow">
+            <h2 class="text-xl font-serif font-bold text-neutral-800">Human-Centered Design & Engineering</h2>
+            <p class="text-sm text-neutral-400 leading-7">Master of Science, University of Washington<br/>
+                User-centered design  <span class="px-4">|</span>  Visual communication  <span class="px-4">|</span>  User research</p>
+        </div>
+    </div>
+    <div class="flex gap-6 items-baseline">
+        <div class="text-sm text-neutral-400 flex-grow-0 flex-shrink-0 w-16 leading-7 text-right">Jul 2012<br>Sep 2008</div>
+        <div class="flex-grow">
+            <h2 class="text-xl font-serif font-bold text-neutral-800">Cognitive Science</h2>
+            <p class="text-sm text-neutral-400 leading-7">Bachelor of Engineering, Xiamen University<br/>
+                Computer science  <span class="px-4">|</span>  Artificial intelligence  <span class="px-4">|</span>  Software development</p>
+        </div>
+    </div>
+
+</main>
 
 
 <script lang="ts">
--- portfolio/tailwind.config.cjs
+++ portfolio/tailwind.config.cjs
@@ -3,7 +3,25 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ['Arial',
+        '"Helvetica"',
+        'sans-serif',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Noto Sans"',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',],
+        serif: ['"Times New Roman"'],
+      },  
+    }
   },
   plugins: []
 };
