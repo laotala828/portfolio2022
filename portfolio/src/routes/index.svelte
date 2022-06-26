fa0a70195b4444d18e78ebc4ef30167abe6233b9
laotala828 <laotala@gmail.com>
2022-06-25 22:42:57 -0700

    initiate

---
+++ portfolio/src/routes/index.svelte
@@ -0,0 +1,15 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<script lang="ts">
+    let zxc: [number, string]  = [1,"zxc"]
+
+    zxc.push("zxc")
+    
+    console.log(zxc)
+</script>
+
+<style>
+    
+</style>
