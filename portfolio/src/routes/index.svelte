31f4b439d8563db344cca271c4d4587221361ce8
laotala828 <5688380+laotala828@users.noreply.github.com>
2022-06-26 21:15:10 -0700

    image set

--- portfolio/src/routes/index.svelte
+++ portfolio/src/routes/index.svelte
@@ -4,66 +4,97 @@
         <div class="flex-grow">
             <h1 class=" text-5xl font-serif font-bold text-neutral-800">Lasifu Ta<span class="text-sm ml-4 font-sans font-normal">Product designer</span></h1>
             <div class="flex gap-8 mt-4 text-sm">
-                <p>2066606221</p>
-                <span class="font-light">|</span>
                 <p>laotala@gmail.com</p>
                 <span class="font-light">|</span>
                 <p>linkedin/lasifuta</p>
+                <span class="font-light">|</span>
+                <p>Resume</p>
             </div>
         </div>
     </div>
-    <div class="flex gap-6 items-baseline">
-        <div class="text-sm text-neutral-400 flex-grow-0 flex-shrink-0 w-16 leading-7 text-right relative">Present<br>Nov 2021
-            <div class="absolute -left-28 top-0 text-neutral-800 font-bold text-right">experience</div>
+    <div class="group hover:bg-neutral-900 -m-6 p-6 rounded-md relative overflow-hidden">
+        <div class="h-full absolute right-0 bottom-0 opacity-0 group-hover:opacity-100">
+            <div class="h-full w-full bg-gradient-to-r from-neutral-900 to-transparent absolute"></div>
+            <img class="h-full" src="rewatch.png" alt="rewatch chapter">
         </div>
-        <div class="flex-grow">
-            <h2 class="text-xl font-serif font-bold text-neutral-800">Product designer, Rewatch</h2>
-            <p class="text-sm text-neutral-400 leading-7">Designed the Comment feature to enable async communication on video.<br/>
-                Designed the Series, Chapter, and Tagging features to find content quickly. <br/>
-                Coded the front-end user interface with HTML, CSS, Javascript, and Rails.<br/>
-                Increased Lighthouse accessibility score of the web clients by 40%. </p>
-        </div>
+        <a href="http://www/google.com" class="flex gap-6 items-baseline relative z-50">
+            <div class="text-sm text-neutral-400 flex-grow-0 flex-shrink-0 w-16 leading-7 text-right relative">Present<br>Nov 2021
+                <div class="absolute -left-28 top-0 text-neutral-800 font-bold text-right w-20">experience</div>
+            </div>
+            <div class="flex-grow">
+                <h2 class="text-xl font-serif font-bold text-neutral-800 group-hover:text-neutral-50">Product designer, Rewatch</h2>
+                <p class="text-sm text-neutral-400 leading-7">Designed the Comment feature to enable async communication on video.<br/>
+                    Designed the Series, Chapter, and Tagging features to find content quickly. <br/>
+                    Coded the front-end user interface with HTML, CSS, Javascript, and Rails.<br/>
+                    Increased Lighthouse accessibility score of the web clients by 40%. </p>
+            </div>
+        </a>
     </div>
-    <div class="flex gap-6 items-baseline">
-        <div class="text-sm text-neutral-400 flex-grow-0 flex-shrink-0 w-16 leading-7 text-right">Nov 2021<br>Sep 2019</div>
-        <div class="flex-grow">
-            <h2 class="text-xl font-serif font-bold text-neutral-800">UX designer, Amazon</h2>
-            <p class="text-sm text-neutral-400 leading-7">Led the end-to-end design for AWS BugBust, an AI-enabled gamification experience for bug-fixing. <br/>
-                Designed Amazon DevOps Guru, an AI-driven service to find and predict anomalies.<br/>
-                Modernized the UI of  Amazon Lex, a conversational AI and chatbot service.<br/>
-                Designed AWS Panorama, a computer vision application installed at the edge.</p>
+    <div class="group hover:bg-neutral-900 -m-6 p-6 rounded-md relative overflow-hidden">
+        <div class="h-full absolute right-0 bottom-0 opacity-0 group-hover:opacity-100">
+            <div class="h-full w-full bg-gradient-to-r from-neutral-900 to-transparent absolute"></div>
+            <img class="h-full" src="bugbust.png" alt="aws project bugbust">
         </div>
+        <a href="http://www/google.com" class="flex gap-6 items-baseline relative z-50">
+            <div class="text-sm text-neutral-400 flex-grow-0 flex-shrink-0 w-16 leading-7 text-right">Nov 2021<br>Sep 2019</div>
+            <div class="flex-grow">
+                <h2 class="text-xl font-serif font-bold text-neutral-800 group-hover:text-neutral-50">UX designer, Amazon</h2>
+                <p class="text-sm text-neutral-400 leading-7">Led the end-to-end design for AWS BugBust, an AI-enabled gamification experience for bug-fixing. <br/>
+                    Designed Amazon DevOps Guru, an AI-driven service to find and predict anomalies.<br/>
+                    Modernized the UI of  Amazon Lex, a conversational AI and chatbot service.<br/>
+                    Designed AWS Panorama, a computer vision application installed at the edge.
+                </p>
+            </div>
+        </a>
     </div>
-    <div class="flex gap-6 items-baseline">
-        <div class="text-sm text-neutral-400 flex-grow-0 flex-shrink-0 w-16 leading-7 text-right">Sep 2019<br>Jun 2015</div>
-        <div class="flex-grow">
-            <h2 class="text-xl font-serif font-bold text-neutral-800">Product designer, Microsoft</h2>
-            <p class="text-sm text-neutral-400 leading-7">Led the design for Whiteboard mobile and web clients.<br/>
-                Designed interactions for different input methods and hardware devices.<br/>
-                Integrated Whiteboard with Microsoft Office and Teams ecosystem.<br/>
-                Defined user models to enable secure and smooth cross-tenant collaborations.<br/>
-                Visualized health data to deliver actionable insights on web and mobile devices.</p>
+    <div class="group hover:bg-neutral-900 -m-6 p-6 rounded-md relative overflow-hidden">
+        <div class="h-full absolute right-0 bottom-0 opacity-0 group-hover:opacity-100">
+            <div class="h-full w-full bg-gradient-to-r from-neutral-900 to-transparent absolute z-10"></div>
+            <img class="h-full translate-x-24" src="wb.png" alt="microsoft whiteboard">
         </div>
+        <a href="http://www/google.com" class="flex gap-6 items-baseline relative z-50">
+            <div class="text-sm text-neutral-400 flex-grow-0 flex-shrink-0 w-16 leading-7 text-right">Sep 2019<br>Jun 2015</div>
+            <div class="flex-grow">
+                <h2 class="text-xl font-serif font-bold text-neutral-800 group-hover:text-neutral-50">Product designer, Microsoft</h2>
+                <p class="text-sm text-neutral-400 leading-7">Led the design for Whiteboard mobile and web clients.<br/>
+                    Designed interactions for different input methods and hardware devices.<br/>
+                    Integrated Whiteboard with Microsoft Office and Teams ecosystem.<br/>
+                    Defined user models to enable secure and smooth cross-tenant collaborations.<br/>
+                    Visualized health data to deliver actionable insights on web and mobile devices.</p>
+            </div>
+        </a>
     </div>
-    <div class="flex gap-6 items-baseline">
-        <div class="text-sm text-neutral-400 flex-grow-0 flex-shrink-0 w-16 leading-7 text-right relative">May 2015<br>Sep 2013
-            <div class="absolute -left-28 top-0 text-neutral-800 font-bold text-right">education</div>
+    <div class=" group hover:bg-neutral-900 -m-6 p-6 rounded-md relative overflow-hidden">
+        <div class="h-full absolute right-0 bottom-0 opacity-0 group-hover:opacity-100">
+            <div class="h-full w-full bg-gradient-to-r from-neutral-900 to-transparent absolute"></div>
+            <img class="h-full" src="uw.png" alt="university of washington">
         </div>
-        <div class="flex-grow">
-            <h2 class="text-xl font-serif font-bold text-neutral-800">Human-Centered Design & Engineering</h2>
-            <p class="text-sm text-neutral-400 leading-7">Master of Science, University of Washington<br/>
-                User-centered design  <span class="px-4">|</span>  Visual communication  <span class="px-4">|</span>  User research</p>
-        </div>
+        <a href="https://www.hcde.washington.edu/" target="_blank" class="flex gap-6 items-baseline relative z-50">
+            <div class="text-sm text-neutral-400 flex-grow-0 flex-shrink-0 w-16 leading-7 text-right relative">May 2015<br>Sep 2013
+                <div class="absolute -left-28 top-0 text-neutral-800 font-bold text-right w-20">education</div>
+            </div>
+            <div class="flex-grow">
+                <h2 class="text-xl font-serif font-bold text-neutral-800 group-hover:text-neutral-50">Human-Centered Design & Engineering</h2>
+                <p class="text-sm text-neutral-400 leading-7">Master of Science, University of Washington<br/>
+                    User-centered design  <span class="px-4">|</span>  Visual communication  <span class="px-4">|</span>  User research</p>
+            </div>
+        </a>
     </div>
-    <div class="flex gap-6 items-baseline">
-        <div class="text-sm text-neutral-400 flex-grow-0 flex-shrink-0 w-16 leading-7 text-right">Jul 2012<br>Sep 2008</div>
-        <div class="flex-grow">
-            <h2 class="text-xl font-serif font-bold text-neutral-800">Cognitive Science</h2>
-            <p class="text-sm text-neutral-400 leading-7">Bachelor of Engineering, Xiamen University<br/>
-                Computer science  <span class="px-4">|</span>  Artificial intelligence  <span class="px-4">|</span>  Software development</p>
+    <div class=" group hover:bg-neutral-900 -m-6 p-6 rounded-md relative overflow-hidden">
+        <div class="h-full absolute right-0 bottom-0 opacity-0 group-hover:opacity-100 ">
+            <div class="h-full w-full bg-gradient-to-r from-neutral-900 to-transparent absolute z-10"></div>
+            <img class="h-full" src="xmu.png" alt="xiamen university">
         </div>
+        <a href="https://informatics.xmu.edu.cn/en/index.htm" target="_blank" class="flex gap-6 items-baseline relative z-50">
+            <div class="text-sm text-neutral-400 flex-grow-0 flex-shrink-0 w-16 leading-7 text-right relative">Jul 2012<br>Sep 2008
+            </div>
+            <div class="flex-grow">
+                <h2 class="text-xl font-serif font-bold text-neutral-800 group-hover:text-neutral-50">Cognitive Science</h2>
+                <p class="text-sm text-neutral-400 leading-7">Bachelor of Engineering, Xiamen University<br/>
+                    Computer science  <span class="px-4">|</span>  Artificial intelligence  <span class="px-4">|</span>  Software development</p>
+            </div>
+        </a>
     </div>
-
 </main>
 
 
